8cae812b086f3b0b9f183009c40b93c71c554106
Dominic Garms <djgarms@gmail.com>
2020-09-10 13:45:25 +0800

    remove ignored files

--- node_modules/json-schema-to-typescript/dist/src/validator.d.ts
+++
@@ -1,2 +0,0 @@
-import { JSONSchema } from './types/JSONSchema';
-export declare function validate(schema: JSONSchema, filename: string): string[];
--- node_modules/json-schema-to-typescript/src/formatter.ts
+++
@@ -1,6 +0,0 @@
-import {format as prettify} from 'prettier'
-import {Options} from './'
-
-export function format(code: string, options: Options): string {
-  return prettify(code, {parser: 'typescript', ...options.style})
-}
